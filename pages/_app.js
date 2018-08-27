f95247e6ed832e3049fbbed47b92237cf2deee57
mbektimirov <mbektimirov@gmail.com>
2018-08-27 21:34:30 +0300

    Material jss styling for next.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,11 @@ import { Provider } from 'react-redux'
 import withRedux from 'next-redux-wrapper'
 import withReduxSaga from 'next-redux-saga'
 
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import JssProvider from 'react-jss/lib/JssProvider'
+import getPageContext from '../src/getPageContext'
+
 import createStore from '../store'
 
 class MyApp extends App {
@@ -17,12 +22,45 @@ class MyApp extends App {
     return { pageProps }
   }
 
+  constructor(props) {
+    super(props)
+    this.pageContext = getPageContext()
+  }
+
+  pageContext = null
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }
+
   render() {
     const { Component, pageProps, store } = this.props
     return (
       <Container>
         <Provider store={store}>
-          <Component {...pageProps} />
+          {/* Wrap every page in Jss and Theme providers */}
+          <JssProvider
+            registry={this.pageContext.sheetsRegistry}
+            generateClassName={this.pageContext.generateClassName}
+          >
+            {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+            <MuiThemeProvider
+              theme={this.pageContext.theme}
+              sheetsManager={this.pageContext.sheetsManager}
+            >
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              {/* Pass pageContext to the _document though the renderPage enhancer
+                to render collected styles on server side. */}
+              <Component pageContext={this.pageContext} {...pageProps} />
+            </MuiThemeProvider>
+          </JssProvider>
         </Provider>
       </Container>
     )
